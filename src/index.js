2d05da5b5b7b4494ef745e01bac1246d542ef83b
AndreyAndreyQ11 <aayy6491@gmail.com>
2025-02-05 23:12:01 +0200

    урок 3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
 import Lesson_1 from "./Lesson_1/Lesson_1";
 import Lesson_2 from "./Lesson_2/Lesson_2";
+import Lesson_3 from "./Lesson_3/Lesson_3";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Lesson_1 />
     <Lesson_2 />
+    <Lesson_3 />
   </React.StrictMode>
 );
